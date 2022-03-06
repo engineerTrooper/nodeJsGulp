7305d588ac5a446a858c6f8f7d4ba8d0cb01c8bf
EngineerTrooper <kontakt@engineertrooper.com>
2022-03-06 20:44:18 +0100

    SASS and SCSS file compression for KirbyCMS
    SASS, SCSS compressed to CSS; four Methods; updateting Readme-File

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nodeJsGulp
-My NodeJs Gulp scripts to get tasks in process automated.
+My NodeJs Gulp scripts to get tasks in processes automated.
+
+These Scripts I am using in my projects with gulp.
+Use them as you like, share them, ... whatever.
+
+I woulde be glad to get recommendation and a backlink to my website or this repositiry.
+
+I am also happy about suggestion and improvments via pull request.
+[www.engineertrooper.com](https://engineertrooper.com)
---
+++ gulpfile-KirbyCms.js
@@ -0,0 +1,62 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const debug = require("gulp-debug");  //debug rule if debug should be shown
+
+
+var originSass = "../assets/sass/**/*.sass";
+var originScss = "../assets/sass/**/*.scss";
+var destination = "../assets/css/";
+var destinationUncompressed = "../assets/uncompressed/css";
+
+
+function ScssToCss() {
+    return(
+        gulp
+            .src(originScss)
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .on("error", sass.logError)
+            .pipe(gulp.dest(destination))
+    );
+};
+
+function ScssToCssUncompressed() {
+    return(
+        gulp
+            .src(originScss)
+            .pipe(sass().on('error', sass.logError))
+            .on("error", sass.logError)
+            .pipe(gulp.dest(destinationUncompressed))
+    );
+};
+
+function SassToCss() {
+    return(
+        gulp
+            .src(originSass)
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .on("error", sass.logError)
+            .pipe(gulp.dest(destination))
+    );
+};
+
+function SassToCssUncompressed() {
+    return(
+        gulp
+            .src(originSass)
+            .pipe(sass().on('error', sass.logError))
+            .on("error", sass.logError)
+            .pipe(gulp.dest(destinationUncompressed))
+    );
+};
+
+exports.ScssToCss = ScssToCss;
+exports.SassToCss = SassToCss;
+exports.ScssToCssUncompressed = ScssToCssUncompressed;
+exports.SassToCssUncompressed = SassToCssUncompressed;
+exports.watch = function () {
+    gulp.watch(originScss, gulp.series('ScssToCss'));
+    gulp.watch(originSass, gulp.series('SassToCss'));
+    gulp.watch(originScss, gulp.series('ScssToCssUncompressed'));
+    gulp.watch(originSass, gulp.series('SassToCssUncompressed'));
+};
